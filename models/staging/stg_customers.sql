7949c1c34273e88863ee13c0177fadcba27a08cc
DataSpell-demo <you@example.com>
2024-01-17 12:47:32 +0100

    Change materialization type in stg_customers model
    The change modifies the materialization type for stg_customers model from 'view' to 'table'. The modification was necessary to improve the performance and efficiency of the model in the database system.

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,3 +1,3 @@
-{{  config(materialized='view') }}
+{{  config(materialized='table') }}
 
 select customer_id, first_name, last_name, address.address, address.postal_code from {{ source('dvd_store_data', 'customer')}} left join address on customer.address_id = address.address_id
